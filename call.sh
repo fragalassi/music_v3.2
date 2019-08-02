d761ea566f70ebdd4df5787fe3b18a66ba4d4edb
francesca.galassi@inria.fr <francesca.galassi@inria.fr>
2019-08-02 12:29:53 +0200

    ms models

--- call.sh
+++ call.sh
@@ -11,7 +11,7 @@ for patient in $DATADIR/*; do
 	FLAIR=$DATADIR/"$patientID"/3DFLAIR.nii.gz
 	T1=$DATADIR/"$patientID"/3DT1.nii.gz
 
-	python3 animaMSExamPreparation.py -r $FLAIR -f $FLAIR -t $T1  -o $DATADIR/"$patientID"/ 
+	PYTHONHASHSEED=0 python3 animaMSExamPreparation.py -r $FLAIR -f $FLAIR -t $T1  -o $DATADIR/"$patientID"/ 
 
 	#ADDITIONAL PREPROCESSING
 	#training/testing MICCAI16
@@ -20,7 +20,7 @@ for patient in $DATADIR/*; do
 	MASK=$DATADIR/"$patientID"/3DFLAIR_brainMask.nrrd
 	C=$DATADIR/"$patientID"/Consensus.nii.gz
 
-	python3 animaMusicLesionSegmentation_v3.py -f $FLAIR -t $T1 -m $MASK -n 6 -o $DATADIR/"$patientID"/
+	PYTHONHASHSEED=0 python3 animaMusicLesionSegmentation_v3.py -f $FLAIR -t $T1 -m $MASK -n 6 -o $DATADIR/"$patientID"/
 
     fi
 done
